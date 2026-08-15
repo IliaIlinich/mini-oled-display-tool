dc3d144e614d8c77140c0eb442974677939ebcef
IliaIlinich <ilya.ilinich@gmail.com>
2026-08-15 18:02:22 +0200

    Added option to clear the display Small code improvements

--- mini_oled_info.cpp
+++ mini_oled_info.cpp
@@ -22,40 +22,49 @@ static void printUsage(const char* progName) {
          << "  -i, --ip      IP Address\n"
          << "  -u, --uptime  Uptime string\n"
          << "  -d, --date    Date and Time string\n"
+         << "  -c, --clear   Clear the display and exit\n"
          << "  -h, --help    Show this help message\n";
 }
 
-int main(int argc, char** argv) {
+int main(const int argc, char** argv) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     // Default values if arguments are omitted
     string hostname = "Unknown";
     string ip = "No IP";
     string uptime = "0d 00h 00m";
     string dateAndTime = "YYYY-MM-DD HH:MM:SS";
+    bool clearOnly = false; // Flag to check if we only want to clear
 
     // Setup command line options
+    // Note: Changed has_arg to match short option behavior (required_argument vs no_argument)
     static struct option long_options[] = {
-        {"name",   required_argument, 0, 'n'},
-        {"ip",     required_argument, 0, 'i'},
-        {"uptime", required_argument, 0, 'u'},
-        {"date",   required_argument, 0, 'd'},
-        {"help",   no_argument,       0, 'h'},
-        {0, 0, 0, 0}
+        {.name = "name",   .has_arg = required_argument, .flag = nullptr, .val = 'n'},
+        {.name = "ip",     .has_arg = required_argument, .flag = nullptr, .val = 'i'},
+        {.name = "uptime", .has_arg = required_argument, .flag = nullptr, .val = 'u'},
+        {.name = "date",   .has_arg = required_argument, .flag = nullptr, .val = 'd'},
+        {.name = "clear",  .has_arg = no_argument,       .flag = nullptr, .val = 'c'},
+        {.name = "help",   .has_arg = no_argument,       .flag = nullptr, .val = 'h'},
+        {.name = nullptr,  .has_arg = 0,                 .flag = nullptr, .val = 0}
     };
 
     int opt;
     int option_index = 0;
-    while ((opt = getopt_long(argc, argv, "n:i:u:d:h", long_options, &option_index)) != -1) {
+    // Added 'c' to the short options string
+    while ((opt = getopt_long(argc, argv, "n:i:u:d:hc", long_options, &option_index)) != -1) {
         switch (opt) {
             case 'n': hostname = optarg; break;
             case 'i': ip = optarg; break;
             case 'u': uptime = optarg; break;
             case 'd': dateAndTime = optarg; break;
-            case 'h': printUsage(argv[0]); return 0; // Printing usage page with -h
-            default:  printUsage(argv[0]); return 1; // Printing usage page if invalid argument has bit entered
+            case 'c': clearOnly = true; break; // Set flag instead of calling clear right away
+            case 'h': printUsage(argv[0]); return 0;
+            default:  printUsage(argv[0]); return 1;
         }
     }
 
-    // Initialize the display
+    // 1. Initialize the display FIRST
     if (!display.init(OLED_I2C_RESET, OLED_ADAFRUIT_I2C_128x64)) {
         cerr << "Failed to initialize OLED display." << endl;
         return 1;
@@ -63,8 +72,15 @@ int main(int argc, char** argv) {
 
     display.begin();
 
-    // Clear the buffer from the PREVIOUS execution
+    // 2. Clear the memory buffer
     display.clearDisplay();
+
+    // 3. If -c was passed, push the empty buffer and exit immediately
+    if (clearOnly) {
+        display.display(); // This physically blanks the screen
+        display.close();
+        return 0;
+    }
 
     // Render the Top Bar
     display.setTextColor(BLACK, WHITE);
